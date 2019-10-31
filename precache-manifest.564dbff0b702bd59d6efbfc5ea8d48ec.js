5532fbddaf34bb1d7ad648f74fd3e249ae88a2d8
Aizat Faiz <aizat.faiz@gmail.com>
2019-10-31 08:06:35 +0800

    Updates

---
+++ precache-manifest.564dbff0b702bd59d6efbfc5ea8d48ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfeb98644c303e2c7aa0a4f3ef2007fb",
+    "url": "/golden-hour/index.html"
+  },
+  {
+    "revision": "43a7686bf5135c4b175a",
+    "url": "/golden-hour/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "adc20ecf157c2e5e3be1",
+    "url": "/golden-hour/static/js/2.c068dd6c.chunk.js"
+  },
+  {
+    "revision": "43a7686bf5135c4b175a",
+    "url": "/golden-hour/static/js/main.509a1c47.chunk.js"
+  },
+  {
+    "revision": "f7bd69f696dddf42272e",
+    "url": "/golden-hour/static/js/runtime-main.fe4e6846.js"
+  }
+]);
